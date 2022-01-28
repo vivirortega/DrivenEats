035a7d751d6774d08ca0c187946dfd1f0acde790
unverzed <vitoriarortega@gmail.com>
2022-01-28 15:11:26 -0300

    implementado ao selecionar um card da mesma categoria os outros ficam desselecionados

--- scripts.js
+++ scripts.js
@@ -2,40 +2,58 @@
 
 function chooseLasagna() {
     document.querySelector(".lasagna").style.border = "4px solid green";
+    document.querySelector(".pasta").style.border = "4px solid white";
+    document.querySelector(".gnocchi").style.border = "4px solid white";
 }
 
 function chooseGnocchi() {
     document.querySelector(".gnocchi").style.border = "4px solid green";
+    document.querySelector(".pasta").style.border = "4px solid white";
+    document.querySelector(".lasagna").style.border = "4px solid white";
 }
 
 function choosePasta() {
     document.querySelector(".pasta").style.border = "4px solid green";
+    document.querySelector(".gnocchi").style.border = "4px solid white";
+    document.querySelector(".lasagna").style.border = "4px solid white";
 }
 
 //bebidas
 
 function chooseJuice() {
     document.querySelector(".juice").style.border = "4px solid green";
+    document.querySelector(".cola").style.border = "4px solid white";
+    document.querySelector(".fanta").style.border = "4px solid white";
 }
 
 function chooseCola() {
     document.querySelector(".cola").style.border = "4px solid green";
+    document.querySelector(".fanta").style.border = "4px solid white";
+    document.querySelector(".juice").style.border = "4px solid white";
 }
 
 function chooseFanta() {
     document.querySelector(".fanta").style.border = "4px solid green";
+    document.querySelector(".cola").style.border = "4px solid white";
+    document.querySelector(".juice").style.border = "4px solid white";
 }
 
 //sobremesa
 
 function choosePie() {
     document.querySelector(".pie").style.border = "4px solid green";
+    document.querySelector(".eclair").style.border = "4px solid white";
+    document.querySelector(".brownie").style.border = "4px solid white";
 }
 
 function chooseEclair() {
     document.querySelector(".eclair").style.border = "4px solid green";
+    document.querySelector(".brownie").style.border = "4px solid white";
+    document.querySelector(".pie").style.border = "4px solid white";
 }
 
 function chooseBrownie() {
     document.querySelector(".brownie").style.border = "4px solid green";
+    document.querySelector(".pie").style.border = "4px solid white";
+    document.querySelector(".eclair").style.border = "4px solid white";
 }
