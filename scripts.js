49775b5e00d123551c45167bb34f9661db4a34f3
unverzed <vitoriarortega@gmail.com>
2022-01-28 14:10:28 -0300

    colocando borda verde nos cards

---
+++ scripts.js
@@ -0,0 +1,41 @@
+// pratos
+
+function chooseLasagna() {
+    document.querySelector(".lasagna").style.border = "4px solid green";
+}
+
+function chooseGnocchi() {
+    document.querySelector(".gnocchi").style.border = "4px solid green";
+}
+
+function choosePasta() {
+    document.querySelector(".pasta").style.border = "4px solid green";
+}
+
+//bebidas
+
+function chooseJuice() {
+    document.querySelector(".juice").style.border = "4px solid green";
+}
+
+function chooseCola() {
+    document.querySelector(".cola").style.border = "4px solid green";
+}
+
+function chooseFanta() {
+    document.querySelector(".fanta").style.border = "4px solid green";
+}
+
+//sobremesa
+
+function choosePie() {
+    document.querySelector(".pie").style.border = "4px solid green";
+}
+
+function chooseEclair() {
+    document.querySelector(".eclair").style.border = "4px solid green";
+}
+
+function chooseBrownie() {
+    document.querySelector(".brownie").style.border = "4px solid green";
+}
